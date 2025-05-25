247dd2c11b9d3db1c46ab885a77fdf56eee78fe0
Alex D <a_p_dauria@hotmail.com>
2025-05-25 17:30:39 +0200

    TP Funciones terminado.

--- 05 Funciones/DAURIA_TP-05.py
+++ 05 Funciones/DAURIA_TP-05.py
@@ -1,86 +1,205 @@
-# Práctico 5: Listas - Alex Dauria
+# Práctico 5: Funciones - Alex Dauria
 
 # ACTIVIDAD 1)
 
-# Creamos una lista, utilizando el método range, mostrando los múltiplos de 4. Comenzamos con el 4, que se incluye, y el stop es 101, ya que ese no se incluiría
-multiplos_de_4= list(range(4, 101, 4))
-print(multiplos_de_4)
+# Definir Funciones
+def imprimir_hola_mundo():
+    print("Hola Mundo!")
+
+# Programa Principal
+imprimir_hola_mundo()
 
 # ACTIVIDAD 2)
 
-# Creamos una lista con 5 elementos
-lista_5_elementos= ["Hola", 2, "Mundo", "Hola Mundo!", 10]
-# Imprimimos el anteúltimo accediendo al final de la lista con los índices negativos
-print(lista_5_elementos[-2])
+# Definir Funciones
+def saludar_usuario(nombre):
+    return f"Hola {nombre}!"
+
+# Programa Principal
+nombre = input("Cual es tu nombre? ")
+print(saludar_usuario(nombre))
 
 # ACTIVIDAD 3)
 
-# Creamos la lista vacía
-lista_vacia  = []
-lista_vacia.append("Agrego")
-lista_vacia.append("tres")
-lista_vacia.append("palabras")
-# Imprimimos el resultado actualizado
-print(lista_vacia)
+# Definir Funciones
+def informacion_personal(nombre, apellido, edad, residencia):
+    print(f"Soy {nombre} {apellido}, tengo {edad} años y vivo en {residencia}")
 
-# ACTIVIDAD 4)
+# Programa Principal
+name = input("Nombre: ")
+surname = input("Apellido: ")
+age = input("Edad: ")
+home = input("Residencia: ")
 
-animales = ["perro","gato", "conejo", "pez"]
-# Reemplazamos el segundo valor
-animales[1] = "loro"
-# Reemplazamos el último valor
-animales[-1] = "oso"
-# Imprimimos el resultado actualizado
-print(animales)
+informacion_personal(name, surname, age, home)
+
+# ACTIVIDAD 4) 
+
+import math
+# Definir Funciones
+def calcular_area_circulo(radio):
+    area = math.pi * radio**2
+    return area
+
+def calcular_perimetro_circulo(radio):
+    perimetro = 2 * math.pi * radio
+    return perimetro
+
+def imprimir_resultados(area, perimetro):
+    print(f"Area del circulo: {area:.2f}")
+    print(f"Perimetro del circulo: {perimetro:.2f}")
+
+# Programa Principal
+radio = float(input("Radio del circulo: "))
+imprimir_resultados(calcular_area_circulo(radio), calcular_perimetro_circulo(radio))
 
 # ACTIVIDAD 5)
 
-# El programa mencionado crea una lista con números en la primera línea, luego elimina al de mayor valor
-# (en este caso 22), e imprime la lista actualizada
+# Definir Funciones
+def segundos_a_horas(segundos):
+    horas = segundos / 3600
+    return horas
 
-# ACTIVIDAD 6)
+def imprimir_rta(segundos, horas):
+    print(f"{segundos} segundos equivalen a {horas:.1f} horas.")
 
-# Creamos la lista, incluyendo el 30
-lista_10al30 = list(range(10, 31, 5))
-# Imprimimos los 2 primeros valores
-print(lista_10al30[0:2])
+# Programa Principal
+seg = int(input("Segundos: "))
+imprimir_rta(seg, segundos_a_horas(seg))
 
-# ACTIVIDAD 7) 
+# ACTIVIDAD 6) 
 
-autos = ["sedan", "polo", "suran", "gol"]
-# Accedemos a los indices deseados y los reemplazamos por otros dos
-autos[1:3] = ["clio", "mercedes"]
-# Imprimimos el resultado
-print(autos)
+# Definir Funciones
+def tabla_multiplicar(numero):
+    for i in range(1, 11):
+        resultado = numero * i
+        print(f"{numero} * {i} = {resultado}")
 
-# ACTIVIDAD 8)
+        
+# Programa Principal
+tabla_multiplicar(int(input("Tabla del número: ")))
 
-# Creamos la lista vacía
-dobles = []
-# Agregamos el doble de 5, 10 y 15
-dobles.append(5*2)
-dobles.append(10*2)
-dobles.append(15*2)
-# Imprimimos el resultado
-print(dobles)
+
+# ACTIVIDAD 7)
+
+# Definir Funciones
+def operaciones_basicas(a, b):
+    suma = a + b
+    resta = a - b
+    multiplicacion = a * b
+    division = a / b if b != 0 else None  # None si no se puede dividir
+    return (suma, resta, multiplicacion, division)
+
+# Programa Principal
+
+num1 = int(input("Número 1: "))
+num2 = int(input("Número 2: "))
+
+resultados = operaciones_basicas(num1, num2)
+
+print(f"{num1} + {num2} = {resultados[0]}")
+print(f"{num1} - {num2} = {resultados[1]}")
+print(f"{num1} * {num2} = {resultados[2]}")
+
+if resultados[3] is not None:
+    print(f"{num1} / {num2} = {resultados[3]:.1f}")
+else:
+    print("Error en la división. No se puede dividir por 0.")
+
+# ACTIVIDAD 8) 
+
+# Definir Funciones
+def calcular_imc(peso, altura):
+    imc = peso / ( altura ** 2)
+    print(f"Indice de masa corporal (IMC): {imc:.2f}")
+    if imc < 18.5:
+        print("Clasificación: Bajo peso")
+    elif 18.5 <= imc < 25:
+        print("Clasificación: Normal")
+    elif 25 <= imc < 30:
+        print("Clasificación: Sobrepeso")
+    elif 30 <= imc < 35:
+        print("Clasificación: Obesidad grado I")
+    elif 35 <= imc < 40:
+        print("Clasificación: Obesidad grado II")
+    else:
+        print("Clasificación: Obesidad grado III (mórbida)")
+
+# Programa Principal
+while True:
+    try:
+        peso = float(input("Peso en kilogramos: "))
+        if peso <= 0:
+            print("Ingrese un número mayor a 0.")
+        else:
+            break
+    except ValueError:
+        print("Ingrese un número válido.")
+
+while True:
+    try:
+        altura = float(input("Altura en metros: "))
+        if altura <= 0:
+            print("Ingrese un número mayor a 0.")
+        else:
+            break
+    except ValueError:
+        print("Ingrese un número válido.")
+
+calcular_imc(peso, altura)
 
 # ACTIVIDAD 9)
 
-# Lista de compras
-compras = [["pan", "leche"], ["arroz", "fideos", "salsa"], ["agua"]]
-# Agregamos jugo al tercer cliente
-compras[2].append("jugo")
-# Reemplazamos fideos por tallarines en el segundo cliente
-compras[1][1] = "tallarines"
-# Eliminamos pan del primer cliente
-compras[0].remove("pan")
-# Imprimimos resultado actualizado
-print(compras)
+# Definir Funciones
+def celsius_a_fahrenheit(celsius):
+    fahrenheit = (celsius * (9/5)) + 32
+    print(f"{celsius}° Celsius = {fahrenheit:.2f}° Fahrenheit.")
+
+# Programa Principal
+
+while True:
+    try:
+        celsius = float(input("Temperatura en Celsius: "))
+        break
+    except ValueError:
+        print("Ingrese un número válido.")
+
+celsius_a_fahrenheit(celsius)
 
 # ACTIVIDAD 10)
 
-# Creamos la lista anidada con los datos recibidos
-lista_anidada = [15, True, [25.5, 57.9, 30.6], False]
+# Definir Funciones
+def calcular_promedio(a, b, c):
+    promedio = (a + b + c) / 3
+    print(f"El promedio es: {promedio}.")
 
-#Imprimimos el resultado
-print(lista_anidada)
+# Programa Principal
+
+print("¡Calculemos el promedio!")
+
+while True:
+    try:
+        num1 = float(input("Número 1: "))
+        break
+    except ValueError:
+        print("Ingrese un número válido.")
+
+while True:
+    try:
+        num2 = float(input("Número 2: "))
+        break
+    except ValueError:
+        print("Ingrese un número válido.")
+
+while True:
+    try:
+        num3 = float(input("Número 3: "))
+        break
+    except ValueError:
+        print("Ingrese un número válido.")
+
+calcular_promedio(num1, num2, num3)
+
+
+
+
